646533302da0584c5c7366210118d0e211687fe9
fatemaAlbasri <falbasri4@gmail.com>
2025-08-09 01:47:01 +0300

    fatema/ adding sign up page and form , addind API , connect to backend

--- luqma/src/App.jsx
+++ luqma/src/App.jsx
@@ -4,9 +4,19 @@ import Cart from './pages/Cart'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 import Navbar from './components/Navbar'
+import SignUp from './pages/SignUp'
+
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
