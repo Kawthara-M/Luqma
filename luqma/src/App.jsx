b572ea9e5e10a3cd4d0b5475088d40c82d1a6677
Yara-Waleed <yarawaleed292@gmail.com>
2025-08-09 12:50:14 +0300

    added import

--- luqma/src/App.jsx
+++ luqma/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import Profile from './pages/Profile'
 import Navbar from './components/Navbar'
 import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 
 import { Route, Routes } from 'react-router-dom'
 
