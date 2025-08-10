4eede665064d7b214d1f2b9cf20b31e5d5d30359
Yara-Waleed <yarawaleed292@gmail.com>
2025-08-10 14:30:57 +0300

    did the nav bar

--- luqma/src/App.jsx
+++ luqma/src/App.jsx
@@ -1,21 +1,21 @@
-import "./App.css"
-import { useState, useEffect } from "react"
-import Ckechout from "./pages/Checkout"
-import Cart from "./pages/Cart"
-import Home from "./pages/Home"
-import Profile from "./pages/Profile"
-import Navbar from "./components/Navbar"
-import SignUp from "./pages/SignUp"
-import SignIn from "./pages/SignIn"
-import RestaurantsList from "./components/RestaurantsList"
-import { Route, Routes } from "react-router-dom"
-import Restaurant from "./components/Restuatant"
-import { CheckSession } from "./services/Auth"
+import './App.css'
+import { useState, useEffect } from 'react'
+import Ckechout from './pages/Checkout'
+import Cart from './pages/Cart'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Navbar from './components/Navbar'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import RestaurantsList from './components/RestaurantsList'
+import { Route, Routes } from 'react-router-dom'
+import Restaurant from './components/Restuatant'
+import { CheckSession } from './services/Auth'
 
 function App() {
   const [customer, setCustomer] = useState(null)
 
-/*   const checkToken = async () => {
+  /*   const checkToken = async () => {
     //If a token exists, sends token to localStorage to persist logged in user
     const userData = await CheckSession()
     setUser(userData)
@@ -36,11 +36,11 @@ function App() {
   }, []) */
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn setCustomer={setCustomer} />} />
-
         <Route path="/profile/:id" element={<Profile />} />
 
         <Route path="/Home" element={<RestaurantsList />} />
