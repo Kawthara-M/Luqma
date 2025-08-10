e6a5869c80ae1d01720b7a7e0d767ef68de96b1a
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-08-10 15:05:49 +0300

    cleaning app

--- luqma/src/App.jsx
+++ luqma/src/App.jsx
@@ -8,7 +8,6 @@ import Profile from './pages/Profile'
 import Navbar from './components/Navbar'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
-import RestaurantsList from './components/RestaurantsList'
 import { Route, Routes } from 'react-router-dom'
 import Restaurant from './pages/Restuatant'
 import { CheckSession } from './services/Auth'
@@ -47,8 +46,6 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/profile/:id" element={<Profile />} />
         <Route path="/restaurants/:id" element={<Restaurant />} />
-
-      
       </Routes>
     </>
   )
