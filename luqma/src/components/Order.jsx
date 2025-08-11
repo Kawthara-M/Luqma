417c6f8bfc591f8786f926034d71851730ab7ca8
fatemaAlbasri <falbasri4@gmail.com>
2025-08-11 11:01:28 +0300

    update resturant name

--- luqma/src/components/Order.jsx
+++ luqma/src/components/Order.jsx
@@ -3,7 +3,7 @@ import RestaurantsList from './RestaurantsList'
 const OrderCard = ({ order }) => {
   return (
     <div>
-      <h3>Restaurant: {order.restaurant}</h3>
+      <h3>Restaurant: {order.restaurant.name}</h3>
       <p>Status: {order.status}</p>
       <p>Address: {order.address}</p>
       <p>Total Price: ${order.totalPrice}</p>
